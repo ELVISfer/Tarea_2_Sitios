316bd297fb8ff596702402e3af0c5f354603cb18
ELVISfer <115681920+ELVISfer@users.noreply.github.com>
2023-10-25 20:05:55 -0600

    sitio

---
+++ pagina/src/App.jsx
@@ -0,0 +1,17 @@
+// src/App.js
+
+import React from 'react';
+import Nav from './Componente/Nav';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+      </div>
+    </Router>
+  );
+  
+}
+
+export default App;
